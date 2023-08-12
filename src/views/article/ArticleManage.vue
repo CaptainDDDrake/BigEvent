2e803578ea684c1c7ebbd16cd15aa4e0b7a136c9
CaptainDDDrake <503400835@qq.com>
2023-08-12 14:54:03 +0800

    搜索和重置-184

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -6,20 +6,50 @@ import { artGetListService } from '@/api/article.js'
 import { formatTime } from '@/utils/format.js'
 const articleList = ref([]) // 文章列表
 const total = ref(0) // 总条数
+const loading = ref(false) // loading状态
+
 // 定义请求参数对象
 const params = ref({
-  pagenum: 1,
-  pagesize: 5,
+  pagenum: 1, // 当前页
+  pagesize: 5, // 当前生效的每页条数
   cate_id: '',
   state: ''
 })
 
 const getArticleList = async () => {
+  loading.value = true
   const res = await artGetListService(params.value)
   articleList.value = res.data.data
   total.value = res.data.total
+  loading.value = false
 }
 getArticleList()
+
+// 处理分页逻辑
+const onSizeChange = (size) => {
+  // console.log('当前每页条数', size)
+  params.value.pagenum = 1
+  params.value.pagesize = size
+  getArticleList()
+}
+const onCurrentChange = (page) => {
+  params.value.pagenum = page
+  // console.log('页码变化了', page)
+  getArticleList()
+}
+
+// 搜索逻辑
+const onSearch = () => {
+  params.value.pagenum = 1 // 重置页面
+  getArticleList()
+}
+// 重置逻辑
+const onReset = () => {
+  params.value.pagenum = 1 // 重置页面
+  params.value.cate_id = ''
+  params.value.state = ''
+  getArticleList()
+}
 
 // 编辑逻辑
 const onEditArticle = (row) => {
@@ -58,13 +88,13 @@ const onDeleteArticle = (row) => {
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">搜索</el-button>
-        <el-button>重置</el-button>
+        <el-button type="primary" @click="onSearch">搜索</el-button>
+        <el-button @click="onReset">重置</el-button>
       </el-form-item>
     </el-form>
 
     <!-- 表格区域 -->
-    <el-table :data="articleList">
+    <el-table :data="articleList" v-loading="loading">
       <el-table-column label="文章标题" prop="title">
         <template #default="{ row }">
           <el-link type="primary" :underline="false">{{ row.title }}</el-link>
@@ -98,6 +128,19 @@ const onDeleteArticle = (row) => {
         </template>
       </el-table-column>
     </el-table>
+
+    <!-- 分页区域 -->
+    <el-pagination
+      v-model:current-page="params.pagenum"
+      v-model:page-size="params.pagesize"
+      :page-sizes="[2, 3, 4, 5, 10]"
+      layout="jumper, total, sizes, prev, pager, next"
+      background
+      :total="total"
+      @size-change="onSizeChange"
+      @current-change="onCurrentChange"
+      style="margin-top: 20px; justify-content: flex-end"
+    />
   </page-container>
 </template>
 
